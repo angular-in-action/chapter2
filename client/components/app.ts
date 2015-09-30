0ca0dabe44f6bc59fcf41eb30ef95201b2146605
Jeremy Wilken <gnomation@gnomeontherun.com>
2015-09-29 19:30:58 -0500

    add routing

--- client/components/app.ts
+++ client/components/app.ts
@@ -1,11 +1,21 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
 import {Component, View} from 'angular2/angular2';
+import {RouteConfig, Router, RouterOutlet, RouterLink} from 'angular2/router';
+
+import {Dashboard} from './dashboard';
+import {Manage} from './manage';
+
+@RouteConfig([
+  {path: '/', as: 'dashboard', component: Dashboard},
+  {path: '/manage', as: 'manage', component: Manage}
+])
 
 @Component({
   selector: 'app'
 })
 @View({
+  directives: [RouterOutlet, RouterLink],
   template: `
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
@@ -13,8 +23,8 @@ import {Component, View} from 'angular2/angular2';
         <span class="mdl-layout-title">Stock Tracker</span>
         <div class="mdl-layout-spacer"></div>
         <nav class="mdl-navigation mdl-layout--large-screen-only">
-          <a class="mdl-navigation__link">Dashboard</a>
-          <a class="mdl-navigation__link">Manage</a>
+          <a class="mdl-navigation__link" [router-link]="['/dashboard']">Dashboard</a>
+          <a class="mdl-navigation__link" [router-link]="['/manage']">Manage</a>
         </nav>
       </div>
     </header>
