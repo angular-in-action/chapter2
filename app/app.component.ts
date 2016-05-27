b0c6594389f7740330a26fe901c5707c97adc397
Jeremy Wilken <gnomation@gnomeontherun.com>
2016-05-27 17:09:46 -0500

    add routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,18 @@
 import {Component} from '@angular/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+
+import {Dashboard} from './dashboard.component';
+import {Manage} from './manage.component';
+
+@RouteConfig([
+  {path: '/', name: 'Dashboard', component: Dashboard},
+  {path: '/manage', name: 'Manage', component: Manage}
+])
 
 @Component({
   selector: 'app',
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS],
   template: `
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
@@ -9,13 +20,13 @@ import {Component} from '@angular/core';
         <span class="mdl-layout-title">Stock Tracker</span>
         <div class="mdl-layout-spacer"></div>
         <nav class="mdl-navigation mdl-layout--large-screen-only">
-          <a class="mdl-navigation__link">Dashboard</a>
-          <a class="mdl-navigation__link">Manage</a>
+          <a class="mdl-navigation__link" [routerLink]="['Dashboard']">Dashboard</a>
+          <a class="mdl-navigation__link" [routerLink]="['Manage']">Manage</a>
         </nav>
       </div>
     </header>
     <main class="mdl-layout__content" style="padding: 20px;">
-
+      <router-outlet></router-outlet>
     </main>
   </div>
   `
--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,6 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
-import {ROUTER_PROVIDERS} from '@angular/router';
 import {HTTP_PROVIDERS} from '@angular/http';
 
 import {App} from './app.component';
 
-bootstrap(App, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
+bootstrap(App, [HTTP_PROVIDERS]);
