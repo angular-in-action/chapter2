d31422625302a41c01f3e34b390c0b347406b0ae
Jeremy Wilken <gnomation@gnomeontherun.com>
2016-09-22 13:51:00 -0700

    add stocks service

---
+++ src/app/services/stocks.service.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+import { Http } from '@angular/http';
+
+let stocks: Array<string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR'];
+let service: string = 'https://angular2-in-action-api.herokuapp.com';
+
+export interface StockInterface {
+  symbol: string;
+  lastTradePriceOnly: number;
+  change: number;
+  changeInPercent: number;
+}
+
+@Injectable()
+export class StocksService {
+  http: Http;
+  constructor(http: Http) {
+    this.http = http;
+  }
+
+  get() {
+    return stocks;
+  }
+
+  add(stock) {
+    stocks.push(stock);
+    return this.get();
+  }
+
+  remove(stock) {
+    stocks.splice(stocks.indexOf(stock), 1);
+    return this.get();
+  }
+
+  load(symbols) {
+    if (symbols) {
+      return this.http.get(service + '/stocks/snapshot?symbols=' + symbols.join());
+    }
+  }
+}
